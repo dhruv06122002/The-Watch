b9fdaf6204fa75117b2881491d4e956129ceb502
DHRUV ITWALA <dhruvitwala@gmail.com>
2023-02-04 16:34:08 +0530

    Update

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene("Game");
+    }
+    public void Settings()
+    {
+        SceneManager.LoadScene("Settings");
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void Selectplayer()
+    {
+        SceneManager.LoadScene("Player");
+    }
+}
